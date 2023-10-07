2847d00f1ad85e63fa0404474f623394183c153e
Andreas Li <andrea10@stud.ntnu.no>
2023-10-07 22:23:55 +0200

    filter popup styled

--- kylling-ris/src/features/food-search/search-options/filter-option-popup.tsx
+++ kylling-ris/src/features/food-search/search-options/filter-option-popup.tsx
@@ -1,21 +1,51 @@
 import styles from "./filter-option-popup.module.css";
+import { useState } from "react";
 
 export default function FilterOptionPopup() {
+  const [modal, setModal] = useState(true);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
   return (
-    <div className={styles.filterOptionsWrapper}>
-      <div className={styles.title}>Alternativer</div>
-      <select className={styles.dropdown} name="sort">
-        <option value="protein-ascending">Proteiner (stigene)</option>
-        <option value="protein-descending">Proteiner (synkene)</option>
-        <option value="kcal-ascending">kalorier (stigene)</option>
-        <option value="kcal-descending">kalorier (synkene)</option>
-      </select>
-      <label>Gluten</label>
-      <input type="checkbox" name="Gluten" id="Gluten" />
-      <label>Melk</label>
-      <input type="checkbox" name="Melk" id="Melk" />
-      <label>Soya</label>
-      <input type="checkbox" name="Soya" id="Soya" />
-    </div>
+    <>
+      {modal && (
+        <div>
+          <div className={styles.overlay} onClick={toggleModal}></div>
+          <div className={styles.filterContent}>
+            <div className={styles.title}>Filter</div>
+
+            <select className={styles.dropdown} name="sort">
+              <option value="name-ascending">Navn a-z</option>
+              <option value="name-descending">Navn z-a</option>
+              <option value="protein-ascending">Proteiner (stigene)</option>
+              <option value="protein-descending">Proteiner (synkene)</option>
+              <option value="kcal-ascending">kalorier (stigene)</option>
+              <option value="kcal-descending">kalorier (synkene)</option>
+            </select>
+
+            <div className={styles.labelCheckboxContainer}>
+              <label>Gluten</label>
+              <input className={styles.checkbox} type="checkbox" name="Gluten" id="Gluten" />
+            </div>
+
+            <div className={styles.labelCheckboxContainer}>
+              <label>Melk</label>
+              <input className={styles.checkbox} type="checkbox" name="Melk" id="Melk" />
+            </div>
+
+            <div className={styles.labelCheckboxContainer}>
+              <label>Soya</label>
+              <input className={styles.checkbox} type="checkbox" name="Soya" id="Soya" />
+            </div>
+
+            <button className={styles.saveOptionsButton} onClick={toggleModal}>
+              Lagre
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
